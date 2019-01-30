c4d5ea9bc6c4839c07eaa4c72bfcf4e9dbe06c27
Chris Arnesen <chris.arnesen@gmail.com>
2019-01-30 11:30:03 -0800

    Update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+// This is the entry point for the `bitcoin-config` command-line interface.
+// The source code is written in TypeScript and compiled to JavaScript.
+
 import { cli } from '@carnesen/cli';
 import { root } from './root';
 
